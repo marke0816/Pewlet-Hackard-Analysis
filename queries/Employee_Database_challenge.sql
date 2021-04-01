3ba922dc7e6b3d8ad4e96fc73f00bd1f7cb41911
Mark Esposito <markanthonyesposito@hotmail.com>
2021-04-01 15:35:17 -0500

    deliv 2 complete

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -34,3 +34,20 @@ GROUP BY unique_titles.title
 ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY (emp_no);
+
